91d9180727b47e2d81f18e7e16bddf59e1590dfe
Thiago Leite <leite.tjs@gmail.com>
2021-07-13 08:11:24 -0300

    Updates

---
+++ precache-manifest.6081d499f948ee5016317aafd4fe97aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76a09ebe9b854bc37ab8761e24645a1",
+    "url": "/crud-movies/index.html"
+  },
+  {
+    "revision": "99bbf201b1d8ae1c6c52",
+    "url": "/crud-movies/static/css/main.58bbef8d.chunk.css"
+  },
+  {
+    "revision": "789452219209260d2076",
+    "url": "/crud-movies/static/js/2.9a58c218.chunk.js"
+  },
+  {
+    "revision": "99bbf201b1d8ae1c6c52",
+    "url": "/crud-movies/static/js/main.09ebe8bb.chunk.js"
+  },
+  {
+    "revision": "782da029e638607b3de2",
+    "url": "/crud-movies/static/js/runtime-main.5e98a9fd.js"
+  }
+]);
